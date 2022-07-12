d0563132a8b6e5b4b7c30660f3cbb85b2fa483b4
Guilherme Figueiredo <guilherme_figueiredo@outlook.com.br>
2022-07-12 17:47:47 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const comidas = [
+    'feijoada', 
+    'macarronada',
+    'pizza',
+    'strogonoff',
+    'peixe assado'
+]
+
+console.log(comidas);
+
+comidas.unshift('Guisado')
+
+console.log(comidas);
+
+comidas.pop()
+
+console.log(comidas);
+
+comidas.splice(5,0,'sushi','peixe cozido')
+
+console.log(comidas);
+
+comidas.splice(0,1)
+
+console.log(comidas);
+
+/////////////////////////
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+
+console.log(numbers);
+
